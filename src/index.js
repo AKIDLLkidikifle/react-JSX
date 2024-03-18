3c5c810e5de8738f98a8af5cb6fccff6eb6d692e
kidistkifle31 <kidistkifle31@gmail.com>
2024-03-18 20:35:58 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import react from "react";
+import reactDom  from "react-dom";
+var currentyear = new Date().getFullYear()
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reactDom.render(
+   <div>
+      <p>this is kidist</p>
+      <p>copy right {currentyear}</p>
+   </div>,
+   document.getElementById("root")
+)
