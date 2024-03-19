153c3a6d1e550e0032953dee0bee0e63618d9cfe
kidistkifle31 <kidistkifle31@gmail.com>
2024-03-19 13:51:54 +0300

    react styling practice with some logic

--- src/index.js
+++ src/index.js
@@ -2,22 +2,28 @@ import React from "react";
 import ReactDom from "react-dom/client";
 
 const root = ReactDom.createRoot(document.getElementById("root"));
-const img = "https://picsum.photos/200/300";
-const customeStyle = {
-      color:"red",
-      fontSize:"20px",
-      border: "1px solid black"
-};
 
-customeStyle.color = "blue";
+const customStyle = {
+    color:"black"
+}
+
+let d = new Date(2019,1,1, 19);
+let time = d.getHours();
+let message = "";
+
+if(time < 12){
+   message = "good morning";
+   customStyle.color="red";
+}
+else if(time < 18){
+   message = "good afternoon";
+   customStyle.color="green";
+}
+else{
+   message = "good night";
+   customStyle.color="blue";
+}
 
 root.render(
-    <div>
-      <h1 style={customeStyle}>i am AKIDLL</h1>
-      <div>
-         <img src={img} alt="" />
-         <img src={img} alt="" />
-         <img src={img} alt="" />
-      </div>
-    </div>
-);
+   <h1 className="heading" style={customStyle}>{message}</h1>
+)
