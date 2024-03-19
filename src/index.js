37f957740e8e12fc857eb13017713eceba2e2c46
kidistkifle31 <kidistkifle31@gmail.com>
2024-03-19 11:19:08 +0300

    inline style for react element

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import React from "react";
-import ReactDom  from "react-dom/client";
+import ReactDom from "react-dom/client";
 
-const img = "https://picsum.photos/200/300";
 const root = ReactDom.createRoot(document.getElementById("root"));
+const img = "https://picsum.photos/200/300";
+const customeStyle = {
+      color:"red",
+      fontSize:"20px",
+      border: "1px solid black"
+};
+
+customeStyle.color = "blue";
 
 root.render(
-   <div>
-     <h1 className="heading">this is react</h1>
-     <div>
-         <img src={img+"?grayscale"} alt=""/>
-         <img src={img+"?grayscale"} alt=""/>
-         <img src={img+"?grayscale"} alt=""/>
-     </div>
-   </div>
+    <div>
+      <h1 style={customeStyle}>i am AKIDLL</h1>
+      <div>
+         <img src={img} alt="" />
+         <img src={img} alt="" />
+         <img src={img} alt="" />
+      </div>
+    </div>
 );
