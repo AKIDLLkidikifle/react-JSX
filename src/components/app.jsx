c31b5e281ba21a9610ce8f64ba75192973e90626
kidistkifle31 <kidistkifle31@gmail.com>
2024-03-20 09:22:45 +0300

    add header, footer and note

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Header from "./header";
+import Footer from "./footer";
+import Note from "./note";
 
 function App(){
-    return <div>
+   return <div>
         <Header/>
-    </div>;
-};
+        <Note/>
+        <Footer/>
+    </div>  
+}
 
 export default App;
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,29 +1,10 @@
 import React from "react";
 
-let time = new Date(2024, 1, 2, 19).getHours();
-let message = "hi";
-let custemStyle = {
-    color:"red"
-}
-
 function Header(){
-    if(time < 12){
-        message = "Good morning";
-        custemStyle.color = "red";
-    }
-    else if(time < 18){
-        message = "Good afternoon";
-        custemStyle.color = "green";
-    }
-    else{
-        message = "Good evening";
-        custemStyle.color = "blue";
-    }
-    return (
-         <div>
-            <h1 className="heading" style={custemStyle}>{message}</h1>
-         </div>
-    )
-};
+   return <header>
+      <h1>keeper app</h1>
+   </header>
+   
+}
 
 export default Header;
