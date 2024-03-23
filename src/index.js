0059490c3c2999652d07269ab77bffefef50e497
kidistkifle31 <kidistkifle31@gmail.com>
2024-03-23 11:49:10 +0300

    Array and object destructuring

--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
+// CHALLENGE: uncomment the code below and see the car stats rendered
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import ReactDOM from "react-dom/client";
+import Practice from "./practice";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const [honda, tesla] = Practice;
 
+const {model: hundamodel, coloursByPopularity:[Hcolor], speedStats:{topSpeed:hundaspeed}} = honda;
 
+const {model: teslamodel, coloursByPopularity:[Tcolor], speedStats:{topSpeed:teslaspeed}} = tesla;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <table>
+    <thead>
+      <th>Brand</th>
+      <th>Top color</th>
+      <th>Top speed</th>
+    </thead>
+    <thead>
+      <td>{teslamodel}</td>
+      <td>{Tcolor}</td>
+      <td>{teslaspeed}</td>
+    </thead>
+    <thead>
+      <td>{hundamodel}</td>
+      <td>{Hcolor}</td>
+      <td>{hundaspeed}</td>
+    </thead>
+  </table>
+);
