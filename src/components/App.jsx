7164dd7189211aca283091a69f5ec96d14930604
kidistkifle31 <kidistkifle31@gmail.com>
2024-03-25 15:13:20 +0300

    js spreed operation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,52 +3,58 @@ import React, {useState} from "react";
 function App() {
 
    const [contact, setcontact] = useState({
-       firstname: "",
-       lastname: "",
+       fname: "",
+       lname: "",
        email:""
    });
 
    function handleOnchange(event){
-            const {value, name} = event.target;
+            const {name, value} = event.target;
 
-            setcontact(prevvalue=>{
-                  if(name==="fname"){
-                       return(
-                        {
-                          firstname:value,
-                          lastname:prevvalue.lastname,
-                          email:prevvalue.email
-                        }
-                       )
-                  }
-                  else if(name==="lname"){
-                       return(
-                        {
-                          firstname:prevvalue.firstname,
-                          lastname:value,
-                          email:prevvalue.email  
-                        }
-                       )
-                  }
-                  else{
-                        return(
-                          {
-                            firstname:prevvalue.firstname,
-                            lastname:prevvalue.lastname,
-                            email:value
-                          }
-                        )
-                  }
+            setcontact(prevvalue => {
+                  return(
+                    { 
+                      ...prevvalue,
+                      [name] : value
+                    }
+                  );
+                  // if(name==="fname"){
+                  //      return(
+                  //       {
+                  //         fname:value,
+                  //       name:prevvalue.lastname,
+                  //         email:prevvalue.email
+                  //       }
+                  //      )
+                  // }
+                  // else if(name==="lname"){
+                  //      return(
+                  //       {
+                  //         fname:prevvalue.firstname,
+                  //         lname:value,
+                  //         email:prevvalue.email  
+                  //       }
+                  //      )
+                  // }
+                  // else{
+                  //       return(
+                  //         {
+                  //           fname:prevvalue.firstname,
+                  //           lname:prevvalue.lastname,
+                  //           email:value
+                  //         }
+                  //       )
+                  // }
             })
    }
 
   return (
     <div className="container">
       <form >
-         <h1>hello {contact.firstname} {contact.lastname}</h1>
+         <h1>hello {contact.fname} {contact.lname}</h1>
          <p>{contact.email}</p>
-         <input name="fname" onChange={handleOnchange} type="text" placeholder="What's your name?" value={contact.firstname}/>
-         <input name="lname" onChange={handleOnchange} type="text" placeholder="What's your father name?" value={contact.lastname}/>
+         <input name="fname" onChange={handleOnchange} type="text" placeholder="What's your name?" value={contact.fname}/>
+         <input name="lname" onChange={handleOnchange} type="text" placeholder="What's your father name?" value={contact.lname}/>
          <input name="email" onChange={handleOnchange} type="email" placeholder="What's your email address" value={contact.email}/>
          <button type="submit">Submit</button>
       </form>
