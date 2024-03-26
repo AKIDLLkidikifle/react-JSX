e3c734a83d66c1d55efb4d4d3b9910af1abd3d18
kidistkifle31 <kidistkifle31@gmail.com>
2024-03-26 12:42:14 +0300

    manage component tree

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,9 @@
 import React, {useState} from "react";
+import TodoItem from "./Todoitem";
 
 function App() {
   const[inputtext, setInputText] = useState("");
   const[Item, setitem] = useState([]);
-
   function handleONchange(event){
       const currentText = event.target.value;
       setInputText(currentText);
@@ -17,23 +17,32 @@ function App() {
     setInputText(" ");
   };
 
+  function deleteItem(id){
+    setitem((prevValue)=>{
+      return prevValue.filter((element, index)=>{
+         return index!==id;
+      })
+    })
+  }
+
   return (
     <div className="container">
       <div className="heading">
-        <h1>To-Do List </h1>
+          <h1>To-Do List </h1>
       </div>
       <div className="form">
-        <input  onChange={handleONchange} type="text" value={inputtext}/>
-        <button onClick={handleClick}> <span>Add</span> </button>
+           <input  onChange={handleONchange} type="text" value={inputtext}/>
+           <button onClick={handleClick}> <span>Add</span> </button>
       </div>
       <div>
-        <ul> {Item.map((element) => (<li>{element}</li>))} </ul>
-        
+           <ul> {Item.map((element, index) => (
+               <TodoItem key={index} id={index} text={element} onChecked={deleteItem}/>
+             ))}
+           </ul>
       </div>
     </div>
   );
 }
 
 export default App;
-
 
